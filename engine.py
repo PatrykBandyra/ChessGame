0036b853d05da6868451636a8f5a5a4406fd7bdd
Patryk Bandyra <patryk.bandyra@gmail.com>
2021-07-15 02:21:53 +0200

    Move validation - part 1

---
+++ engine.py
@@ -0,0 +1,152 @@
+"""
+Stores information about the current state of a chess game. Responsible for determining valid moves and keeping a move log.
+"""
+import numpy as np
+
+
+class GameState:
+    def __init__(self):
+        self.board = np.array(  # 2D array [y, x]
+            [
+                ['bR', 'bN', 'bB', 'bQ', 'bK', 'bB', 'bN', 'bR'],
+                ['bP', 'bP', 'bP', 'bP', 'bP', 'bP', 'bP', 'bP'],
+                ['--', '--', '--', '--', '--', '--', '--', '--'],
+                ['--', '--', '--', '--', '--', '--', '--', '--'],
+                ['--', '--', '--', '--', '--', '--', '--', '--'],
+                ['--', '--', '--', '--', '--', '--', '--', '--'],
+                ['wP', 'wP', 'wP', 'wP', 'wP', 'wP', 'wP', 'wP'],
+                ['wR', 'wN', 'wB', 'wQ', 'wK', 'wB', 'wN', 'wR']
+            ]
+        )
+
+        self.move_functions = {'P': self.get_pawn_moves, 'R': self.get_rook_moves, 'N': self.get_knight_moves,
+                               'B': self.get_bishop_moves, 'Q': self.get_queen_moves, 'K': self.get_king_moves}
+
+        self.white_to_move = True
+        self.move_log = []
+
+    def make_move(self, move):
+        """
+        TODO: castling, pawn promotion
+        """
+        if self.board[move.start_row][move.start_col] != '--':
+            self.board[move.start_row][move.start_col] = '--'
+            self.board[move.end_row][move.end_col] = move.piece_moved
+            self.move_log.append(move)
+            self.white_to_move = not self.white_to_move
+
+    def undo_move(self):
+        if len(self.move_log) != 0:
+            move = self.move_log.pop()
+            self.board[move.start_row][move.start_col] = move.piece_moved
+            self.board[move.end_row][move.end_col] = move.piece_captured
+            self.white_to_move = not self.white_to_move
+
+    def get_valid_moves(self):
+        """
+        All moves considering checks
+        """
+        return self.get_all_possible_moves()
+
+    def get_all_possible_moves(self):
+        """
+        All moves without considering checks
+        """
+        moves = []
+        for row in range(len(self.board)):
+            for column in range(len(self.board[row])):
+                turn = self.board[row][column][0]   # 'b' or 'w'
+                if (turn == 'w' and self.white_to_move) or (turn == 'b' and not self.white_to_move):
+                    piece = self.board[row][column][1]
+                    self.move_functions[piece](row, column, moves)  # Call appropriate function
+        return moves
+
+    def get_pawn_moves(self, row, column, moves):
+        """
+        Gets all the pawn moves for the pawn at given location and adds these moves to the list
+        """
+        if self.white_to_move:
+            if row - 1 >= 0:
+                if self.board[row-1][column] == '--':   # 1 square advance
+                    moves.append(Move((row, column), (row - 1, column), self.board))
+                    if row == len(self.board) - 2 and self.board[row-2][column]:  # 2 squares advance
+                        moves.append(Move((row, column), (row - 2, column), self.board))
+                if column - 1 >= 0:  # Captures to the left
+                    if self.board[row-1][column-1][0] == 'b':   # Enemy piece to capture
+                        moves.append(Move((row, column), (row - 1, column - 1), self.board))
+                if column + 1 <= len(self.board[row])-1:  # Captures to the right
+                    if self.board[row-1][column+1][0] == 'b':   # Enemy piece to capture
+                        moves.append(Move((row, column), (row - 1, column + 1), self.board))
+        else:
+            if row + 1 <= len(self.board) - 1:
+                if self.board[row+1][column] == '--':   # 1 square advance
+                    moves.append(Move((row, column), (row + 1, column), self.board))
+                    if row == 1 and self.board[row+2][column]:  # 2 squares advance
+                        moves.append(Move((row, column), (row + 2, column), self.board))
+                if column - 1 >= 0:  # Captures to left
+                    if self.board[row+1][column-1][0] == 'w':   # Enemy piece to capture
+                        moves.append(Move((row, column), (row + 1, column - 1), self.board))
+                if column + 1 <= len(self.board[row])-1:  # Captures to right
+                    if self.board[row+1][column+1][0] == 'w':   # Enemy piece to capture
+                        moves.append(Move((row, column), (row + 1, column + 1), self.board))
+
+    def get_rook_moves(self, row, column, moves):
+        """
+        Gets all the rook moves for the pawn at given location and adds these moves to the list
+        """
+        directions = ((-1, 0), (0, -1), (1, 0), (0, 1))  # Up, left, down, right
+        enemy_color = 'b' if self.white_to_move else 'w'
+        for direction in directions:
+            for i in range(1, len(self.board)):
+                end_row = row + direction[0] * i
+                end_col = column + direction[1] * i
+                if 0 <= end_row <= len(self.board)-1 and 0 <= end_col <= len(self.board[row])-1:    # On board
+                    end_piece = self.board[end_row][end_col]
+                    if end_piece == '--':   # Empty space - valid
+                        moves.append(Move((row, column), (end_row, end_col), self.board))
+                    elif end_piece[0] == enemy_color:   # Enemy piece - valid
+                        moves.append(Move((row, column), (end_row, end_col), self.board))
+                        break   # Cannot jump over the enemy piece - no need to check further
+                    else:   # Friendly piece - invalid
+                        break
+                else:   # Off board
+                    break
+
+    def get_knight_moves(self, row, column, moves):
+        pass
+
+    def get_bishop_moves(self, row, column, moves):
+        pass
+
+    def get_queen_moves(self, row, column, moves):
+        pass
+
+    def get_king_moves(self, row, column, moves):
+        pass
+
+
+class Move:
+    ranks_to_rows = {'1': 7, '2': 6, '3': 5, '4': 4, '5': 3, '6': 2, '7': 1, '8': 0}
+    rows_to_ranks = {v: k for k, v in ranks_to_rows.items()}
+    files_to_cols = {'a': 0, 'b': 1, 'c': 2, 'd': 3, 'e': 4, 'f': 5, 'g': 6, 'h': 7}
+    cols_to_files = {v: k for k, v in files_to_cols.items()}
+
+    def __init__(self, start_sq, end_sq, board):
+        self.start_row = start_sq[0]
+        self.start_col = start_sq[1]
+        self.end_row = end_sq[0]
+        self.end_col = end_sq[1]
+        self.piece_moved = board[self.start_row][self.start_col]
+        self.piece_captured = board[self.end_row][self.end_col]
+        self.move_id = self.start_row * 1000 + self.start_col * 100 + self.end_row * 10 + self.end_col
+
+    def __eq__(self, other):
+        if isinstance(other, Move):
+            return self.move_id == other.move_id
+
+    def get_chess_notation(self):
+        return self.get_rank_file(self.start_row, self.start_col) + self.get_rank_file(self.end_row, self.end_col)
+
+    def get_rank_file(self, r, c):
+        return self.cols_to_files[c] + self.rows_to_ranks[r]
+
---
+++ main.py
@@ -0,0 +1,179 @@
+"""
+Main file. Handles user input and displays the current game state.
+"""
+import pygame as pg
+from engine import GameState, Move
+
+WIDTH = HEIGHT = 512
+DIMENSION = 8  # Dimensions of a chess board - 8x8
+SQ_SIZE = HEIGHT // DIMENSION
+MAX_FPS = 60
+HOOVERED_SQ_COLOR = (100, 100, 100)
+
+
+class Player:
+    def __init__(self, color):
+        self.color = color
+        self.player_clicks = []  # Keep track of player clicks
+
+
+class Game:
+    def __init__(self):
+        self.game_state = GameState()
+        self.images = {}
+        self.load_images()
+
+        # self.white_player = Player('white')
+        # self.black_player = Player('black')
+
+        pg.init()
+        pg.display.set_caption('Chess Game')
+        pg.display.set_icon(pg.transform.scale(pg.image.load('resources/logo.png'), (32, 32)))
+        self.screen = pg.display.set_mode((WIDTH, HEIGHT))
+
+        self.selected_piece = ()
+        self.location = ()
+        self.col = 0
+        self.row = 0
+        self.hoovered_square = pg.Surface((SQ_SIZE, SQ_SIZE))
+        self.hoovered_square.fill(HOOVERED_SQ_COLOR)
+
+    def load_images(self):
+        """
+        Loads images of chess pieces based on starting state of a chess board.
+        """
+        pieces = set()
+        for row in self.game_state.board:
+            for square in row:
+                if square != '--':
+                    pieces.add(square)
+
+        for piece in pieces:
+            self.images[piece] = pg.transform.scale(pg.image.load(f'resources/figures_images/{piece}.png'), (SQ_SIZE, SQ_SIZE))
+
+    def draw_game_state(self):
+        self.draw_board()
+        self.draw_hoovered_square()
+        if self.selected_piece != ():
+            self.draw_piece_selection()
+        self.draw_pieces()
+
+    def draw_board(self):
+        # Top left square is always light
+        colors = [pg.Color('white'), pg.Color('gray')]
+        for row in range(DIMENSION):
+            for column in range(DIMENSION):
+                color = colors[(row + column) % 2]
+                pg.draw.rect(self.screen, color, pg.Rect(column*SQ_SIZE, row*SQ_SIZE, SQ_SIZE, SQ_SIZE))
+
+    def draw_piece_selection(self):
+        color = pg.Color('gold')
+        pg.draw.rect(self.screen, color, pg.Rect(self.selected_piece[1] * SQ_SIZE,
+                                                 self.selected_piece[0] * SQ_SIZE, SQ_SIZE, SQ_SIZE))
+
+    def draw_hoovered_square(self):
+        self.screen.blit(self.hoovered_square, (self.col*SQ_SIZE, self.row*SQ_SIZE))
+
+    def draw_pieces(self):
+        for row in range(DIMENSION):
+            for column in range(DIMENSION):
+                piece = self.game_state.board[row][column]
+                if piece != '--':
+                    self.screen.blit(self.images[piece], pg.Rect(column*SQ_SIZE, row*SQ_SIZE, SQ_SIZE, SQ_SIZE))
+
+    def start(self):
+        """
+        e.button:
+            1 - left click
+            2 - middle click
+            3 - right click
+            4 - scroll up
+            5 - scroll down
+        """
+        clock = pg.time.Clock()
+        self.screen.fill(pg.Color('white'))
+        running = True
+
+        # Moving by clicking
+        sq_selected = ()    # (row, column)
+        player_clicks = []
+
+        # Moving by dragging
+        dragging = False
+        dragging_sq_selected = ()   # (row, column)
+
+        # Move validation
+        valid_moves = self.game_state.get_valid_moves()
+        move_made = False
+
+        while running:
+
+            self.location = pg.mouse.get_pos()  # (x, y)
+            self.col = self.location[0] // SQ_SIZE
+            self.row = self.location[1] // SQ_SIZE
+
+            for e in pg.event.get():
+
+                # QUIT
+                if e.type == pg.QUIT:
+                    running = False
+
+                # MOUSE CLICKS
+                elif e.type == pg.MOUSEBUTTONUP:
+
+                    # MOVE - left mouse button
+                    if e.button == 1:
+                        if sq_selected == (self.row, self.col):
+                            sq_selected = ()
+                            player_clicks = []
+                            self.selected_piece = ()
+                        else:
+                            sq_selected = (self.row, self.col)
+                            player_clicks.append(sq_selected)
+
+                        if len(player_clicks) == 1:  # After 1st click
+                            # if not self.game_state.is_square_empty(sq_selected):
+                            if not self.game_state.board[sq_selected[0]][sq_selected[1]] == '--':
+                                self.selected_piece = sq_selected
+                            else:
+                                sq_selected = ()
+                                player_clicks = []
+
+                        if len(player_clicks) == 2:  # After 2nd click
+                            move = Move(player_clicks[0], player_clicks[1], self.game_state.board)
+                            print(move.get_chess_notation())
+                            if move in valid_moves:
+                                self.game_state.make_move(move)
+                                move_made = True
+                            sq_selected = ()
+                            player_clicks = []
+                            self.selected_piece = ()
+
+                # DRAGGING
+                elif e.type == pg.MOUSEBUTTONDOWN:
+                    if e.button == 1:
+                        pass
+
+                # KEY HANDLERS
+                elif e.type == pg.KEYDOWN:
+                    if e.key == pg.K_z:
+                        self.game_state.undo_move()
+                        move_made = True
+
+            if move_made:
+                valid_moves = self.game_state.get_valid_moves()
+                move_made = False
+
+            self.draw_game_state()
+
+            clock.tick(MAX_FPS)
+            pg.display.flip()
+
+
+def main():
+    game = Game()
+    game.start()
+
+
+if __name__ == '__main__':
+    main()
